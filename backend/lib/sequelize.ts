23af0aa681a9676f045b1834d5b2503cb72d53bc
Pedro Facanha <pedruluizmf@gmail.com>
2025-08-03 09:24:21 -0700

    add .env and update .gitignore

--- backend/lib/sequelize.ts
+++ backend/lib/sequelize.ts
@@ -1,15 +1,18 @@
 import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+const mysqlPassword = process.env.MYSQL_PASSWORD;
 
 const sequelize = new Sequelize("codeblocks_db", "pfacanha", "MySQL.123!", {
   host: "localhost",
   dialect: "mysql",
 });
 
-try {
-  await sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
-
-export default sequelize;
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
